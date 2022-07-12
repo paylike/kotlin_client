0ce8ccf093cabe0994c2d151aa83de38027215bf
Laszlo Kocsis <kocsislaci95@gmail.com>
2022-07-12 13:54:27 +0200

    feat(PaylikeHosts): made it dataclass

--- kotlin_client/src/main/java/com/github/paylike/kotlin_client/PaylikeHosts.kt
+++ kotlin_client/src/main/java/com/github/paylike/kotlin_client/PaylikeHosts.kt
@@ -3,15 +3,7 @@ package com.github.paylike.kotlin_client
 /**
  * Describes endpoints used.
  */
-class PaylikeHosts() {
-    var api: String = "https://b.paylike.io"
-    var vault: String = "https://vault.paylike.io"
-
-    /**
-     * Creates a new PaylikeHosts from two given URLs.
-     */
-    constructor(api: String, vault: String) : this() {
-        this.api = api
-        this.vault = vault
-    }
-}
+data class PaylikeHosts(
+    var api: String = "https://b.paylike.io",
+    var vault: String = "https://vault.paylike.io",
+)
