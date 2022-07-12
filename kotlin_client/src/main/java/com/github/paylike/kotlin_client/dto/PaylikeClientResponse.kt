ca04127001ceb0b00af621a9acd0648c2a5d2908
Laszlo Kocsis <kocsislaci95@gmail.com>
2022-07-12 14:26:47 +0200

    fix(PaylikeClientResponse): fixed getter functions to be kotlin styled

--- kotlin_client/src/main/java/com/github/paylike/kotlin_client/dto/PaylikeClientResponse.kt
+++ kotlin_client/src/main/java/com/github/paylike/kotlin_client/dto/PaylikeClientResponse.kt
@@ -6,29 +6,32 @@ import java.lang.Exception
  * Describes the client response from the Paylike capture API
  */
 class PaylikeClientResponse(
-    @JvmField val paymentResponse: PaymentResponse?,
+    paymentResponse: PaymentResponse?,
     val isHTML: Boolean,
-    @JvmField val HTMLBody: String?,
+    HTMLBody: String?,
     val hints: List<String> = mutableListOf(),
 ) {
     /**
      * Returns the payment response if not null
      * otherwise throws an exception
      */
-    fun getPaymentResponse(): PaymentResponse {
-        if (paymentResponse == null) {
-            throw Exception("Payment response is null, cannot be acquired")
+    val paymentResponse: PaymentResponse? = paymentResponse
+        get() {
+            if (field == null) {
+                    throw Exception("Payment response is null, cannot be acquired")
+                }
+            return field
         }
-        return paymentResponse
-    }
+
     /**
      * Returns HTML body if not null
      * otherwise throws an exception
      */
-    fun getHTMLBody(): String {
-        if (HTMLBody == null) {
+    val htmlBody: String? = HTMLBody
+        get() {
+            if (field == null) {
             throw Exception("HTMLBody is null, cannot be acquired")
         }
-        return HTMLBody
-    }
+            return field
+        }
 }
