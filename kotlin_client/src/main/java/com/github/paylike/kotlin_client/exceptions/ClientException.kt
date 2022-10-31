de99900f8359644685232c6a11f3dfa2af23712d
Laszlo Kocsis <kocsislaci95@gmail.com>
2022-10-31 11:21:16 +0100

    feat(exception): parent for local exceptions and fix for expiry

---
+++ kotlin_client/src/main/java/com/github/paylike/kotlin_client/exceptions/ClientException.kt
@@ -0,0 +1,3 @@
+package com.github.paylike.kotlin_client.exceptions
+
+open class ClientException : Exception()
--- kotlin_client/src/main/java/com/github/paylike/kotlin_client/exceptions/InvalidExpiryException.kt
+++ kotlin_client/src/main/java/com/github/paylike/kotlin_client/exceptions/InvalidExpiryException.kt
@@ -1,9 +1,7 @@
 package com.github.paylike.kotlin_client.exceptions
 
-import java.lang.Exception
-
 /** In case the expiry set to invalid date we throw this. */
-class InvalidExpiryException(month: Int, year: Int) : Exception() {
+class InvalidExpiryException(month: Int, year: Int) : ClientException() {
     override val message =
         (if (month > 12 || month < 1) "Invalid month: $month. " else "")
             .plus(if (year > 2099 || year < 2000) "Invalid year: $year. " else "")
